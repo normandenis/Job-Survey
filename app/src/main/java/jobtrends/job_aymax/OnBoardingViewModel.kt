6a8b4e6671eb5eb8c871b4e28f787db6088da357
Norman Denis <norman.denis@jobtrends.io>
2018-01-09 11:34:04 +0100

    Init project with Kotlin settings

---
+++ app/src/main/java/jobtrends/job_aymax/OnBoardingViewModel.kt
@@ -0,0 +1,14 @@
+package jobtrends.job_aymax
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardingViewModel : AppCompatActivity()
+{
+
+	protected fun onCreate(savedInstanceState : Bundle)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.on_boarding_view)
+	}
+}
