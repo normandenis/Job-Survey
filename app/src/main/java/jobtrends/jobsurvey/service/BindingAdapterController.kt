2e713501dff7c997628fad090abf720e43322efc
norman <norman.denis@epitech.eu>
2018-04-29 19:20:47 +0200

    Adding input checking errors

--- app/src/main/java/jobtrends/jobsurvey/service/BindingAdapterController.kt
+++ app/src/main/java/jobtrends/jobsurvey/service/BindingAdapterController.kt
@@ -2,18 +2,35 @@ package jobtrends.jobsurvey.service
 
 import android.databinding.BindingAdapter
 import android.databinding.BindingConversion
+import android.databinding.ObservableField
+import android.support.design.widget.TextInputLayout
 import android.view.View
+import android.widget.EditText
 import android.widget.ListAdapter
 import android.widget.ListView
+import android.text.Editable
+import android.text.TextUtils
+
+
 
 @BindingAdapter("app:adapter")
-fun bindItems(view : ListView, viewModel : ListAdapter?)
+fun bindItems(view: ListView, viewModel: ListAdapter?)
 {
   view.adapter = viewModel
 }
 
 @BindingConversion
-fun convertToVisibility(boolean : Boolean) : Int
+fun convertToVisibility(boolean: Boolean): Int
 {
   return if (boolean) View.VISIBLE else View.INVISIBLE
+}
+
+@BindingAdapter("app:errorText")
+fun setErrorMessage(editText: EditText, message: String?)
+{
+  if (message == null || message == "")
+  {
+    return
+  }
+  editText.error = message
 }
--- app/src/main/java/jobtrends/jobsurvey/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/jobtrends/jobsurvey/viewmodel/SignUpViewModel.kt
@@ -2,9 +2,13 @@ package jobtrends.jobsurvey.viewmodel
 
 import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.databinding.ObservableField
+import android.icu.text.SimpleDateFormat
+import android.net.ParseException
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.EditText
 import jobtrends.jobsurvey.R
 import jobtrends.jobsurvey.databinding.SignUpViewBinding
 import jobtrends.jobsurvey.model.User
@@ -12,14 +16,31 @@ import jobtrends.jobsurvey.model.UserModel
 import jobtrends.jobsurvey.service.APIController
 import jobtrends.jobsurvey.service.JsonController
 import jobtrends.jobsurvey.service.serviceController
+import java.util.*
+import javax.mail.internet.AddressException
+import javax.mail.internet.InternetAddress
 
-class SignUpViewModel : AppCompatActivity()
+class SignUpViewModel : AppCompatActivity
 {
   var userModel = serviceController !!.getInstance<UserModel>()
   var user = serviceController !!.getInstance<User>()
   var jsonController = serviceController !!.getInstance<JsonController>()
   var apiController = serviceController !!.getInstance<APIController>()
   private val tag = "SignUpViewModel"
+  var lastnameErrorText = ObservableField<String?>()
+  var firstnameErrorText = ObservableField<String?>()
+  var birthdayErrorText = ObservableField<String?>()
+  var emailErrorText = ObservableField<String?>()
+  var jobErrorText = ObservableField<String?>()
+  var passwordErrorText = ObservableField<String?>()
+  var passwordBisErrorText = ObservableField<String?>()
+  private val funcs = mutableMapOf<Int, (String?) -> Boolean>()
+
+  constructor() : super()
+  {
+    funcs[0] = { s : String? -> isValidEmailAddress(s) }
+    funcs[1] = { s : String? -> isValidFormat(s) }
+  }
 
   override fun onCreate(savedInstanceState : Bundle?)
   {
@@ -28,16 +49,74 @@ class SignUpViewModel : AppCompatActivity()
     binding.vm = this
   }
 
+
+  fun checkInput()
+  {
+    if (!funcs[0]!!(userModel.email.get()))
+    {
+      emailErrorText.set("Invalid email")
+    }
+  }
+
+  fun isValidEmailAddress(email: String?): Boolean
+  {
+    var result = true
+    try
+    {
+      val emailAddr = InternetAddress(email)
+      emailAddr.validate()
+    }
+    catch (ex: AddressException)
+    {
+      result = false
+    }
+
+    return result
+  }
+
+  fun isValidFormat(value: String?): Boolean
+  {
+    val format : String = "dd/MM/yyyy"
+    var date: Date? = null
+    try
+    {
+      val sdf = SimpleDateFormat(format)
+      date = sdf.parse(value)
+      if (value != sdf.format(date))
+      {
+        date = null
+      }
+    }
+    catch (ex: ParseException)
+    {
+      ex.printStackTrace()
+    }
+
+    return date != null
+  }
+
+  private fun isInvalidInput(input : String?): Boolean
+  {
+    if (input == null || input == "")
+    {
+      return true
+    }
+    val regex : Regex = Regex("/^[0-9]{8}[A-Za-z]$/")
+    val result = input.matches(regex)
+    return !result
+  }
+
   fun onClick()
   {
-    user.email = userModel.email.get()
-    user.firstName = userModel.firstName.get()
-    user.lastName = userModel.lastName.get()
-    user.password = userModel.password.get()
-    user.metier = userModel.metier.get()
-    user.birthday = userModel.birthday.get()
-    val json = jsonController.serialize(user)
-    apiController.post("auth/signup", json, ::firstResponse, this)
+    checkInput()
+//    user.email = userModel.email.get()
+//    user.firstName = userModel.firstName.get()
+//    user.lastName = userModel.lastName.get()
+//    user.password = userModel.password.get()
+//    user.metier = userModel.metier.get()
+//    user.birthday = userModel.birthday.get()
+//    val json = jsonController.serialize(user)
+//    apiController.post("auth/signup", json, ::firstResponse, this)
   }
 
   fun firstResponse(code: Int, response : String)
